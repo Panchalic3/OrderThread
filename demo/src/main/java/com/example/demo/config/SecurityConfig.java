6870a32702ae9621d7cfc28e96061da971c7557a
Panchali Chattapadhyay <panchalic3@gmail.com>
2026-01-08 13:13:39 +0530

    spring security 2 - Added the security config file. as of now the following configs are done -                       csrf - its disabled, since we will use jwt, we do not need cookies and session.                       sessionManagement - its set a stateless, since we will use jwt, no need to remember session id and add to cookie, all request will depend on jtw on the       	header.                       authorizeHttpRequests - as of now , its permit all.

---
+++ demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())// disableing the csrf
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
+        /** sessionManagement -> A session is Server-side memory -> You login with username + password
+         Server creates a session - Session ID stored in cookie - Browser sends cookie on every request
+         *
+         * STATELESS = server remembers NOTHING - No session stored on server -No session ID -No server memory per user
+         * Every request is independent
+         * */
+
+        /* think authorizeHttpRequests as gate keeper, in the current scenario,
+        it only allows /auth/login, all other things needs to be authenticated
+        * */
+//                        .authorizeHttpRequests(auth -> auth.anyRequest()
+//                .permitAll()); -> This line permits all
+
+        return http.build();
+    }
+}
