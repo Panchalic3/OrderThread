fcaeed1a020ffffa39f2186879f054d8686b48eb
Panchali Chattapadhyay <panchalic3@gmail.com>
2026-01-09 17:21:13 +0530

    spring security 7.1 - Update the security config class in the following way - add addFilterBefore((jwtFilter, UsernamePasswordAuthenticationFilter.class) to run my JWT filter BEFORE Spring’s default authentication filter.

--- demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -8,10 +8,19 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import com.example.demo.jwtfilter.JwtFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    // added as a part of step 7
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -19,8 +28,12 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())// disableing the csrf
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
+                        .requestMatchers("/auth/login").permitAll()
+                        .anyRequest().authenticated()
+                ) //.addFilterAt replaces the default spring default authentication filter
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);//Run my JWT filter BEFORE Spring’s default authentication filter.
+        // Spring’s default authentication filter - it is the username and password we added in the application.properties
+        // at the very begening, or the spring auto generated password in the console at application start
 
         /** sessionManagement -> A session is Server-side memory -> You login with username + password
          Server creates a session - Session ID stored in cookie - Browser sends cookie on every request
